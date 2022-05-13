4e8a2b0ff9af8b88e2d06fc7dbcad0218e4e232f
TaneriX07 <tanerivince@gmail.com>
2022-05-13 23:28:33 +0800

    Add loader (jumping ball)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loader from './Loader';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
@@ -48,6 +49,7 @@ const Home = () => {
           Create Your Pizza
         </motion.button>
       </Link>
+      <Loader />
     </motion.div>
   );
 };

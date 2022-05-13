2e138bdd53ecb2ad962fcfcdd851bcbce3a7a874
TaneriX07 <tanerivince@gmail.com>
2022-05-13 23:05:26 +0800

    Animate svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,57 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const svgVariants = {
+  start: {
+    rotate: -180,
+  },
+  end: {
+    rotate: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const pathVariants = {
+  start: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  end: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Header = () => {
   return (
     <Link to="/">
       <header>
         <div className="logo">
-          <svg
+          <motion.svg
             className="pizza-svg"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 100 100"
+            variants={svgVariants}
+            initial="start"
+            animate="end"
           >
-            <path
+            <motion.path
               fill="none"
               d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+              variants={pathVariants}
             />
-            <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
-          </svg>
+            <motion.path
+              fill="none"
+              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+              variants={pathVariants}
+            />
+          </motion.svg>
         </div>
         <motion.div
           className="title"
